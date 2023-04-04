e5212660a07952f247c197c1174f33073c15f100
996477559@qq.com <996477559@qq.com>
2023-04-04 19:18:30 +0800

    add stream_chat

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ from transformers import AutoTokenizer, AutoModel
 import torch
 
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(module)s - %(levelname)s - %(message)s')
+
+
 app = Flask(__name__)
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(module)s - %(levelname)s - %(message)s')
 DEVICE = "cuda"
 DEVICE_ID = "0"
 CUDA_DEVICE = f"{DEVICE}:{DEVICE_ID}" if DEVICE_ID else DEVICE
@@ -24,8 +26,13 @@ def torch_gc():
             torch.cuda.ipc_collect()
 
 
+@app.route('/hz')
+def hz():
+    return 'ok'
+
+
 @app.route('/llm/stream_chat', methods=['POST'])
-def chat():
+def llm_chat():
     try:
         request_body = request.get_json()
         prompt = request_body.get('prompt')
@@ -33,21 +40,19 @@ def chat():
         max_length = request_body.get('max_length', 2048)
         top_p = request_body.get('top_p', 0.7)
         temperature = request_body.get('temperature', 0.95)
+
+        for response, history in model.stream_chat(tokenizer, prompt, history, max_length=max_length, top_p=top_p,
+                                                   temperature=temperature):
+            logging.error(response)
+            logging.error(history)
+            now = datetime.datetime.now()
+            time = now.strftime("%Y-%m-%d %H:%M:%S")
+            answer = {
+                "response": response,
+                "history": history,
+                "status": 200,
+                "time": time}
+            yield jsonify(answer)
+        torch_gc()
     except Exception as e:
         logging.error(e)
-
-    for response, history in model.stream_chat(tokenizer, prompt, history, max_length=max_length, top_p=top_p, temperature=temperature):
-        logging.error(response)
-        logging.error(history)
-        now = datetime.datetime.now()
-        time = now.strftime("%Y-%m-%d %H:%M:%S")
-        answer = {
-            "response": response,
-            "history": history,
-            "status": 200,
-            "time": time}
-        yield answer
-    torch_gc()
-
-
-
