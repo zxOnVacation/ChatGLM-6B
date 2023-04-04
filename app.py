2b4611ed995440b4f005bf8a73a1e31c1cf292c1
996477559@qq.com <996477559@qq.com>
2023-04-04 20:04:13 +0800

    add stream_chat

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def infer():
                 "history": history,
                 "status": 200,
                 "time": time}
-            return answer
+            yield answer
 
     torch.cuda.empty_cache()
     torch.cuda.ipc_collect()
