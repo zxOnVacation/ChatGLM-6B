f1609f1893a7e4e8ac804ace60d35444c499c234
996477559@qq.com <996477559@qq.com>
2023-04-04 17:56:07 +0800

    add stream_chat

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from transformers import AutoTokenizer, AutoModel
 import torch
 
 
+app = Flask(__name__)
+
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(module)s - %(levelname)s - %(message)s')
 DEVICE = "cuda"
 DEVICE_ID = "0"
@@ -14,6 +16,7 @@ tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=
 model = AutoModel.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True).half().cuda()
 model.eval()
 
+
 def torch_gc():
     if torch.cuda.is_available():
         with torch.cuda.device(CUDA_DEVICE):
@@ -21,11 +24,8 @@ def torch_gc():
             torch.cuda.ipc_collect()
 
 
-app = Flask(__name__)
-
-
-@app.route('/stream_chat', methods=['POST'])
-def chat(request):
+@app.route('/llm/stream_chat', methods=['POST'])
+def chat():
     try:
         request_body = request.get_json()
         prompt = request_body.get('prompt')
@@ -33,8 +33,8 @@ def chat(request):
         max_length = request_body.get('max_length', 2048)
         top_p = request_body.get('top_p', 0.7)
         temperature = request_body.get('temperature', 0.95)
-    except:
-        raise "please check input params!"
+    except Exception as e:
+        logging.error(e)
 
     for response, history in model.stream_chat(tokenizer, prompt, history, max_length=max_length, top_p=top_p, temperature=temperature):
         logging.error(response)
