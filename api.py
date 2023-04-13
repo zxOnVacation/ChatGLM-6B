7e5251fc98250de1855b8e265cde47f926c81903
996477559@qq.com <996477559@qq.com>
2023-04-13 14:19:23 +0800

    use sse

--- api.py
+++ api.py
@@ -57,6 +57,7 @@ async def llm_stream(item: Item):
                                                    top_p=top_p, temperature=temperature):
                 text = response[len(initial_string):]
                 initial_string = response
+                print(text.encode('utf-8'))
                 yield text
             torch_gc()
             yield '[DONE]'
